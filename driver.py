979ec3d909047953852ff6e1c79dc06f192a9b7f
Mervin Praison <praison.ms@gmail.com>
2019-09-29 23:34:36 +0100

    Update driver.py

--- driver.py
+++ driver.py
@@ -15,7 +15,7 @@ iframe = driver.find_elements_by_tag_name('iframe')
 for frame in iframe:
     print(frame.size)
     print(frame.text)
-    frame.screenshot_as_png
+    print(frame.screenshot_as_png('/var/www/html/adsdetector'))
 print(driver.title)
 driver.quit()
 
